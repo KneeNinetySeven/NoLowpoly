e73abcf3739dcf8d9f334e81e62decaca75197a1
KneeNinetySeven <nils.kneemeyer.i15a@nordakademie.org>
2016-10-02 23:04:48 +0200

    Verschiedene Scenen. Vorbereitungen für Überführung aus Testmodus zum Spieldesgin.

--- src/org/knee/nonopoly/Main.java
+++ src/org/knee/nonopoly/Main.java
@@ -3,6 +3,7 @@ package org.knee.nonopoly;
 import org.knee.nonopoly.entities.spielerStrategien.AllesKaeufer;
 import org.knee.nonopoly.entities.spielerStrategien.StrassenMogul;
 import org.knee.nonopoly.logik.Schiedsrichter;
+import org.knee.nonopoly.ui.MainStage;
 
 /**
  * Created by Nils on 09.09.2016.
@@ -10,9 +11,11 @@ import org.knee.nonopoly.logik.Schiedsrichter;
 public class Main {
 
     Schiedsrichter schiedsrichter;
+    MainStage ui;
 
     public Main() {
-        this.schiedsrichter = new Schiedsrichter();
+        schiedsrichter = new Schiedsrichter();
+        this.ui = new MainStage();
     }
 
     public static void  main(String[] args){
@@ -22,7 +25,5 @@ public class Main {
     public void runGame(){
         schiedsrichter.registriereTeilnehmer("nils", new AllesKaeufer());
         schiedsrichter.registriereTeilnehmer("Matze", new StrassenMogul());
-        System.out.println(schiedsrichter.getBank().toString());
-        schiedsrichter.getProtokollant().printSpieler(schiedsrichter.getTeilnehmer());
     }
 }
