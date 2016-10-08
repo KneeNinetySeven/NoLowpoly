29e86c2d395cd8e76916568e3442c9975784c3d1
KneeNinetySeven <nils.kneemeyer.i15a@nordakademie.org>
2016-10-08 20:30:18 +0200

    KartenTypen

--- src/org/knee/nonopoly/felder/abstracts/KartenFeld.java
+++ src/org/knee/nonopoly/felder/abstracts/KartenFeld.java
@@ -1,23 +1,31 @@
 package org.knee.nonopoly.felder.abstracts;
 
 import org.knee.nonopoly.felder.implementations.FeldTypen;
+import org.knee.nonopoly.felder.implementations.kartenFelder.KartenTypen;
 
 /**
  * Created by Nils on 24.09.2016.
  */
-public abstract class KartenFeld {
+public abstract class KartenFeld extends Feld {
     private int index;
     private String name;
+    public KartenTypen kartenTyp;
 
     public KartenFeld(int index, String name) {
         this.index = index;
         this.name = name;
         this.typ = FeldTypen.KARTENFELD;
+        this.kartenTyp = KartenTypen.ABSTRACT;
+    }
+
+    public boolean istKartenTyp(KartenTypen testKartenTyp){
+        return this.kartenTyp == testKartenTyp;
     }
 
     public int getIndex() {
         return index;
     }
+
     public void setIndex(int index) {
         this.index = index;
     }
---
+++ src/org/knee/nonopoly/felder/implementations/kartenFelder/KartenTypen.java
@@ -0,0 +1,9 @@
+package org.knee.nonopoly.felder.implementations.kartenFelder;
+
+/**
+ * Created by Nils on 08.10.2016.
+ * Package: org.knee.nonopoly.felder.implementations.kartenFelder
+ */
+public enum KartenTypen {
+    GEMEINSCHAFTSFELD, EREIGNISFELD, ABSTRACT
+}
