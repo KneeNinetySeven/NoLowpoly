a0564b0e75f41fc9b378a8cfc08325aa6d4e0e48
KneeNinetySeven <nils.kneemeyer.i15a@nordakademie.org>
2016-10-15 09:32:45 +0200

    Änderungen

--- src/org/knee/nonopoly/felder/immobilien/Strasse.java
+++ src/org/knee/nonopoly/felder/immobilien/Strasse.java
@@ -1,5 +1,9 @@
 package org.knee.nonopoly.felder.immobilien;
 
+import org.knee.nonopoly.entities.Bank;
+import org.knee.nonopoly.entities.Spieler;
+import org.knee.nonopoly.logik.Schiedsrichter;
+
 import java.util.List;
 
 /**
@@ -7,6 +11,7 @@ import java.util.List;
  */
 public class Strasse extends ImmobilienFeld {
 
+    private int hausanzahl;
     private List<Integer> mietStaffel;
     int hauspreis;
 
@@ -15,6 +20,35 @@ public class Strasse extends ImmobilienFeld {
         this.immobilienTyp = ImmobilienTypen.STRASSE;
         this.mietStaffel = mietStaffel;
         this.hauspreis = hauspreis;
+    }
+
+    @Override
+    public void fuehrePflichtAktionAus(Schiedsrichter schiedsrichter) {
+        Spieler aktiverSpieler = schiedsrichter.getAktiverSpieler();
+        if (this.besitzer == schiedsrichter.getBank()){
+            if(aktiverSpieler.getStrategie().erlaubtFeldKauf(aktiverSpieler, this)){
+                wirdGekauftDurchSpieler(aktiverSpieler, schiedsrichter.getBank());
+            }
+        } else if(this.besitzer == aktiverSpieler){
+            if(aktiverSpieler.getStrategie().erlaubtHausbau(aktiverSpieler, this) & getHausanzahl() < 6){
+                wirdNeuBebaut(aktiverSpieler, schiedsrichter.getBank());
+            }
+        } else {
+
+        }
+    }
+
+    public void wirdGekauftDurchSpieler(Spieler spieler, Bank bank){
+        spieler.ueberweiseAn(getKaufpreis(), bank);
+        besitzer = spieler;
+    }
+
+    public void wirdNeuBebaut(Spieler spieler, Bank bank){
+
+    }
+
+    public void baueHaus(){
+        this.setHausanzahl(getHausanzahl() + 1);
     }
 
     @Override
@@ -27,5 +61,21 @@ public class Strasse extends ImmobilienFeld {
         sb.append(", hauspreis=").append(this.hauspreis);
         sb.append('}');
         return sb.toString();
+    }
+
+    public int getHausanzahl() {
+        return hausanzahl;
+    }
+
+    public void setHausanzahl(int hausanzahl) {
+        this.hausanzahl = hausanzahl;
+    }
+
+    public int getHauspreis() {
+        return hauspreis;
+    }
+
+    public void setHauspreis(int hauspreis) {
+        this.hauspreis = hauspreis;
     }
 }
