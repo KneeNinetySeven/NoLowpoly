ecddbcbfe634b9928484e14ab892027d23d3e256
adri94 <adrian.stoelken@nordakademie.de>
2016-10-18 22:30:27 +0200

    neuer Branch für Karten-Implementierung

--- src/org/knee/nonopoly/karten/Karte.java
+++ src/org/knee/nonopoly/karten/Karte.java
@@ -3,7 +3,8 @@ package org.knee.nonopoly.karten;
 /**
  * Created by Nils on 24.09.2016.
  *
- * Karte ist das Interface zur Implementierung von verschiedenen Karten
+ * Karte ist das Interface zur Implementierung von verschiedenen Karten.
+ * TODO: Karten implementieren
  *
  */
 public interface Karte {
