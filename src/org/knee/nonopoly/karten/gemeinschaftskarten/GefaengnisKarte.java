aa11055cb44407ad9c4154e13dcbe2669f5f606c
KneeNinetySeven <nils.kneemeyer.i15a@nordakademie.org>
2016-10-23 14:34:11 +0200

    Merge der Karten in die Felder

--- src/org/knee/nonopoly/karten/gemeinschaftskarten/GefaengnisKarte.java
+++ src/org/knee/nonopoly/karten/gemeinschaftskarten/GefaengnisKarte.java
@@ -7,8 +7,21 @@ import org.knee.nonopoly.logik.Schiedsrichter;
  * Created by Nils on 24.09.2016.
  */
 public class GefaengnisKarte implements Karte {
+
+    private boolean istVergeben = false;
+
     @Override
     public void fuehreKartenAktionAus(Schiedsrichter schiedsrichter) {
+        if(!this.istVergeben){
+            schiedsrichter.getAktiverSpieler().setGefaengnisFreiKarte(this);
+            this.istVergeben = true;
+        } else {
+            //schiedsrichter.naechsteGemeinschaftskarte();
+            schiedsrichter.getProtokollant().printAs("Die Gefängniskarte wurde schon vergeben. Gebe nächste Karte aus...");
+        }
+    }
 
+    public void abgeben(){
+        this.istVergeben = false;
     }
 }
